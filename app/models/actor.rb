7016a29ace9b1f5819bc8d4c78db682edaac58e4
KeslieBox <kesliebox@gmail.com>
2021-02-16 16:55:30 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full_name = "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    Character.find_each do |r|
+        roles << "#{r.name} - #{r.show.name}"
+    end
+    roles
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    actor_list = []
+    Actor.find_each do |c|
+        actor_list << c.full_name
+    end
+    actor_list
+  end
 end
